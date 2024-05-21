08d484d556acf50f2a463d370928668e833ab55e
Gabriel Henrique Machado Magri <mach3dogabrielhenrique@gmail.com>
2024-05-21 20:27:20 -0300

    tabela tela inicial integrada com a tabela de produtos

--- codigo/assets/javascript/Json/pagamento.js
+++ codigo/assets/javascript/Json/pagamento.js
@@ -9,120 +9,116 @@ function salvarpagamentocred() {
         const nomecomple = document.getElementById('nome-completo').value;
         // const parcelame = document.getElementById('validationCustom04').value;
         const numcard = document.getElementById('numero-cartao').value;
+        if (nocart.length >= 5 && valid.length == 4 && cvvv.length == 3 || cvvv == 4 && nomecomple > 10 && numcard.length == 16) {
+            const pagamentoinfo = {
+                "nocart": nocart,
+                "valid": valid,
+                "cvvv": cvvv,
+                "nomecomple": nomecomple,
+                // "parcelame": parcelame,
+                "numcard": numcard,
+                "formadepagamento": "Crédito"
+            }
 
-        const pagamentoinfo = {
-            "nocart": nocart,
-            "valid": valid,
-            "cvvv": cvvv,
-            "nomecomple": nomecomple,
-            // "parcelame": parcelame,
-            "numcard": numcard,
-            "formadepagamento": "Crédito"
+            localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
+            if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
+                localStorage.setItem('confirmacao', 'false');
+            }
+            else {
+                localStorage.setItem('confirmacao', 'true');
+            }
+            alert("Pagamento Realizado com Sucesso!");
+            alert("Você comprou um plano mensal, lembre-se que a renovação automatica está ativada!");
+            window.location.href = 'telaempres.html';
+        } else {
+            alert("Não foi possível prosseguir com o pagamento, verifique as informações e tente novamente!")
         }
 
-        localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
-        if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
-            localStorage.setItem('confirmacao', 'false');
-        }
-        else {
-            localStorage.setItem('confirmacao', 'true');
-        }
-        alert("Pagamento Realizado com Sucesso!");
-        alert("Você comprou um plano mensal, lembre-se que a renovação automatica está ativada!");
-        window.location.href = 'telaempres.html';
+
 
     } else {
-        const nocart = document.getElementById('nome-cartao').value;
-        const valid = document.getElementById('validade').value;
-        const cvvv = document.getElementById('cvvvv').value;
-        const nomecomple = document.getElementById('nome-completo').value;
-        const parcelame = document.getElementById('validationCustom04').value;
-        const numcard = document.getElementById('numero-cartao').value;
+        if (nocart.length >= 5 && valid.length == 4 && cvvv.length == 3 || cvvv == 4 && nomecomple > 10 && numcard.length == 16) {
+            const pagamentoinfo = {
+                "nocart": nocart,
+                "valid": valid,
+                "cvvv": cvvv,
+                "nomecomple": nomecomple,
+                "parcelame": parcelame,
+                "numcard": numcard,
+                "formadepagamento": "Crédito"
+            }
 
-        const pagamentoinfo = {
-            "nocart": nocart,
-            "valid": valid,
-            "cvvv": cvvv,
-            "nomecomple": nomecomple,
-            "parcelame": parcelame,
-            "numcard": numcard,
-            "formadepagamento": "Crédito"
+            localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
+            if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
+                localStorage.setItem('confirmacao', 'false');
+            }
+            else {
+                localStorage.setItem('confirmacao', 'true');
+            }
+            alert("Pagamento Realizado com Sucesso!");
+            alert("Você comprou um plano Anual, lembre-se que a renovação automatica está ativada!");
+            window.location.href = 'telaempres.html';
+        } else {
+            alert("Não foi possível prosseguir com o pagamento, verifique as informações e tente novamente!")
         }
 
-        localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
-        alert("Pagamento Realizado com Sucesso!");
-        alert("Você comprou um plano Anual, lembre-se que a renovação automatica está ativada!");
-        if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
-            localStorage.setItem('confirmacao', 'false');
-        }
-        else {
-            localStorage.setItem('confirmacao', 'true');
-        }
-        window.location.href = 'telaempres.html';
+
     }
-
-
 }
 
 function salvarpagamentodeb() {
     if (planoEscolhido == 'avancadomensal' || planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
-        const nocart = document.getElementById('nome-cartao').value;
-        const valid = document.getElementById('validade').value;
-        const cvvv = document.getElementById('cvvvv').value;
-        const nomecomple = document.getElementById('nome-completo').value;
-        // const parcelame = document.getElementById('validationCustom04').value;
-        const numcard = document.getElementById('numero-cartao').value;
+        if (nocart.length >= 5 && valid.length == 4 && cvvv.length == 3 || cvvv == 4 && nomecomple > 10 && numcard.length == 16) {
+            const pagamentoinfo = {
+                "nocart": nocart,
+                "valid": valid,
+                "cvvv": cvvv,
+                "nomecomple": nomecomple,
+                // "parcelame": parcelame,
+                "numcard": numcard,
+                "formadepagamento": "Débito"
+            }
 
-        const pagamentoinfo = {
-            "nocart": nocart,
-            "valid": valid,
-            "cvvv": cvvv,
-            "nomecomple": nomecomple,
-            // "parcelame": parcelame,
-            "numcard": numcard,
-            "formadepagamento": "Débito"
+            localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
+            if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
+                localStorage.setItem('confirmacao', 'false');
+            }
+            else {
+                localStorage.setItem('confirmacao', 'true');
+            }
+            alert("Pagamento Realizado com Sucesso!");
+            alert("Você comprou um plano mensal, lembre-se que a renovação automatica está ativada!");
+            window.location.href = 'telaempres.html';
+        } else {
+            alert("Não foi possível prosseguir com o pagamento, verifique as informações e tente novamente!")
         }
-
-        localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
-        alert("Pagamento Realizado com Sucesso!");
-        alert("Você comprou um plano mensal, lembre-se que a renovação automatica está ativada!");
-        if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
-            localStorage.setItem('confirmacao', 'false');
-        }
-        else {
-            localStorage.setItem('confirmacao', 'true');
-        }
-        window.location.href = 'telaempres.html';
 
     } else {
-        const nocart = document.getElementById('nome-cartao').value;
-        const valid = document.getElementById('validade').value;
-        const cvvv = document.getElementById('cvvvv').value;
-        const nomecomple = document.getElementById('nome-completo').value;
-        // const parcelame = document.getElementById('validationCustom04').value;
-        const numcard = document.getElementById('numero-cartao').value;
+        if (nocart.length >= 5 && valid.length == 4 && cvvv.length == 3 || cvvv == 4 && nomecomple > 10 && numcard.length == 16) {
+            const pagamentoinfo = {
+                "nocart": nocart,
+                "valid": valid,
+                "cvvv": cvvv,
+                "nomecomple": nomecomple,
+                // "parcelame": parcelame,
+                "numcard": numcard,
+                "formadepagamento": "Crédito"
+            }
 
-        const pagamentoinfo = {
-            "nocart": nocart,
-            "valid": valid,
-            "cvvv": cvvv,
-            "nomecomple": nomecomple,
-            // "parcelame": parcelame,
-            "numcard": numcard,
-            "formadepagamento": "Débito"
+            localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
+            if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
+                localStorage.setItem('confirmacao', 'false');
+            }
+            else {
+                localStorage.setItem('confirmacao', 'true');
+            }
+            alert("Pagamento Realizado com Sucesso!");
+            alert("Você comprou um plano mensal, lembre-se que a renovação automatica está ativada!");
+            window.location.href = 'telaempres.html';
+        } else {
+            alert("Não foi possível prosseguir com o pagamento, verifique as informações e tente novamente!")
         }
 
-        localStorage.setItem('pagamentoinfo', JSON.stringify(pagamentoinfo));
-        alert("Pagamento Realizado com Sucesso! Debito");
-        alert("Você comprou um plano Anual, lembre-se que a renovação automatica está ativada!");
-        if (planoEscolhido == 'mediomensal' || planoEscolhido == 'basicomensal') {
-            localStorage.setItem('confirmacao', 'false');
-        }
-        else {
-            localStorage.setItem('confirmacao', 'true');
-        }
-        window.location.href = 'telaempres.html';
+
     }
-
-
 }
